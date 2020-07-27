3f0b3ea8927b330acf6b8ab02b226b042baa0105
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-07-27 15:30:05 +0300

    Add test endpoints for GET/POST/PUT/PATCH/DELETE verbs, with/without body and with/without respose

---
+++ src/CSharp.ApiRequest.Foo/Dtos/ResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.ApiRequest.Foo.Dtos
+{
+    public sealed class ResponseDto
+    {
+        public ResponseDto()
+        {
+        }
+
+        public ResponseDto(string httpVerb, string text)
+        {
+            HttpVerb = httpVerb;
+            Text = text;
+        }
+
+        public string HttpVerb { get; set; }
+        public string Text { get; set; }
+    }
+}
