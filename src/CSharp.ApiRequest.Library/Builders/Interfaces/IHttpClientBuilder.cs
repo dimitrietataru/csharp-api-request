4cdc0e9fe2976318f5d67828389851cc87d9136b
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-07-29 14:17:53 +0300

    Add HttpClientBuilder - Create HttpClient instance based on RequestInfo metadata

---
+++ src/CSharp.ApiRequest.Library/Builders/Interfaces/IHttpClientBuilder.cs
@@ -0,0 +1,10 @@
+﻿using CSharp.ApiRequest.Library.Structure.Interfaces;
+using System.Net.Http;
+
+namespace CSharp.ApiRequest.Library.Builders.Interfaces
+{
+    public interface IHttpClientBuilder
+    {
+        HttpClient Create(IRequestInfo requestInfo);
+    }
+}
