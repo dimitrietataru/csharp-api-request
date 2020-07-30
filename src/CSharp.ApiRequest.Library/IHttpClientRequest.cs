a66db9156ccf0ff6bf7dc3b2d23cc72d6f572a9f
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-07-30 14:37:05 +0300

    Add missing methods on HttpClientRequest

--- src/CSharp.ApiRequest.Library/IHttpClientRequest.cs
+++ src/CSharp.ApiRequest.Library/IHttpClientRequest.cs
@@ -10,11 +10,14 @@ namespace CSharp.ApiRequest.Library
         Task<TResult> GetAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
         Task<TResult> GetAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
+        Task PostAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
         Task PostAsync<TRequest>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
         Task<TResult> PostAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
         Task<TResult> PostAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
+        Task PutAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
         Task PutAsync<TRequest>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task<TResult> PutAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
         Task<TResult> PutAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
         Task PatchAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
